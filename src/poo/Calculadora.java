779308817aabccb330fe3bb2e5d7bd5baad9cc51
ccamilocampos <ccampos@consultorestecnologicos.net>
2026-02-05 12:59:29 -0500

    CAMBIOS EN CALCULADORA

--- src/poo/Calculadora.java
+++ src/poo/Calculadora.java
@@ -1,4 +1,12 @@
 package poo;
 
-public class calculadora {
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
 }
