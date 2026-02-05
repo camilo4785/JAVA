e520f21a9bc965db0136056418cfba998166cf1f
ccamilocampos <ccampos@consultorestecnologicos.net>
2026-02-05 15:06:04 -0500

    MODIFICACION PERSONA

--- src/poo/Persona.java
+++ src/poo/Persona.java
@@ -1,4 +1,33 @@
 package poo;
 
 public class Persona {
+
+    String nombre;
+    int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void saludar(){
+        System.out.println("Hola, " + nombre);
+    }
+
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 }
